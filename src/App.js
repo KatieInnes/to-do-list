d503d2242228af2a34de512f25c7b5b240b3b558
KatieInnes <katharine.innes@gmail.com>
2023-02-28 13:55:21 -0800

    finalizing project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,77 @@
 import './index.css';
 import React, { useState } from 'react';
-import Form from './components/Form';
-import Display from './components/Display';
-    
 
 function App() {
-  const [toDo, setToDo] = useState([]);
-  return (
-    <div className="App">
-      <Form toDo={ toDo } setToDo={ setToDo } />
-      <Display toDo={ toDo } />
+  const [createToDo, setCreateToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const handleNewSubmit = (e) => {
+    e.preventDefault();
+
+    const listItem = {
+      text: createToDo,
+      complete: false
+    };
+
+    setToDos([...toDos, listItem])
+    setCreateToDo("")
+  };
+
+  const handleDelete = (delIdx) => {
+    const filteredToDos = toDos.filter((toDo, i) => {
+      return i !== delIdx;
+    });
+    setToDos(filteredToDos);
+  };
+
+  const handleComplete = (idx) => {
+    const completedToDos = toDos.map((toDo, i) => {
+      if (idx === i) {
+        const updatedToDo = {...toDo, complete: !toDo.complete};
+        return updatedToDo
+      } else {
+        return toDo;
+      }
+    });
+
+    setToDos(completedToDos);
+  }
+
+  return(
+    <div className="list">
+      <h1>To Do List</h1>
+      <form onSubmit = {(e) => {
+        handleNewSubmit(e);
+      }}>
+        <input onChange={(e) => {
+          setCreateToDo(e.target.value);
+        }} 
+          type="text"
+          value={createToDo} />
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
+
+      {toDos.map((toDo, i) => {
+        const toDoStyles = [];
+          if (toDo.complete) {
+            toDoStyles.push("strikethru")
+          }
+        return(
+          <div key={i}>
+            <input onChange = {(e) => {
+              handleComplete(i);
+            }} checked = {toDo.complete} type="checkbox" />
+            <span className= {toDoStyles.join(" ")}>{toDo.text}</span>
+            <button className="button" onClick = {(event) => {
+              handleDelete(i);
+            }}>Delete</button>
+          </div>
+        );
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -25,26 +25,26 @@ export default Display
 
 
 
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
     
-const ColorDisplay = (props) => {
-    const { currentColor } = props;
+// const ColorDisplay = (props) => {
+//     const { currentColor } = props;
 
-    return (
-        <>
-            {
-                currentColor.map((color, index) => (
-                    <div key={index} 
-                    style={{display: "inline-block",
-                            margin: "15px",
-                            height: "100px",
-                            width: "100px",
-                            backgroundColor: color}}>
-                    </div>
-                ))
-            }
-        </>
-    );
-};
+//     return (
+//         <>
+//             {
+//                 currentColor.map((color, index) => (
+//                     <div key={index} 
+//                     style={{display: "inline-block",
+//                             margin: "15px",
+//                             height: "100px",
+//                             width: "100px",
+//                             backgroundColor: color}}>
+//                     </div>
+//                 ))
+//             }
+//         </>
+//     );
+// };
     
-export default ColorDisplay;
+// export default ColorDisplay;
